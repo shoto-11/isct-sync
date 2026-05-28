a2c3617460ae118e9ae3c963df18454f24337704
ShogoMorita <morita.s.am@m.titech.ac.jp>
2026-05-28 11:05:13 +0900

    fix:speed表示

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
